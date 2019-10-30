c4e732f7b9a3ab7ac06126250ac43f9435b30d76
Pacasian <pacasianisland@gmail.com>
2019-10-31 00:10:50 +0530

    tweets

---
+++ app.py
@@ -0,0 +1,186 @@
+from flask import Flask, render_template,make_response, request, redirect, url_for
+import sys,tweepy,csv,re
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+
+
+li=[]
+class SentimentAnalysis:
+
+    def __init__(self):
+        self.tweets = []
+        self.tweetText = []
+
+    def DownloadData(self,st,nft):
+        # authenticating
+        consumerKey = 'voLvYiLhrmwVlbS4HVtZpUS6L'
+        consumerSecret = 'SV42glPzfN7fNOvDvO7nM3zn5ojFoLqeAFBr0K54WzSSsNvOle'
+        accessToken = '1115660484965834752-bnugBQhRtd0VNBqMqS2RPGmjBR0fOT'
+        accessTokenSecret = 'FXY4EUBnGyiK6jHUgc5d6soFxIlb55b0ODWWIgrrlo9cR'
+        auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+        auth.set_access_token(accessToken, accessTokenSecret)
+        api = tweepy.API(auth)
+
+        # input for term to be searched and how many tweets to search
+        searchTerm = st
+        #input("Enter Keyword/Tag to search about: ")
+        NoOfTerms = int(nft)
+        #int(input("Enter how many tweets to search: "))
+
+        # searching for tweets
+        self.tweets = tweepy.Cursor(api.search, q=searchTerm, lang = "en").items(NoOfTerms)
+
+        # Open/create a file to append data to
+        csvFile = open('result.csv', 'a')
+
+        # Use csv writer
+        csvWriter = csv.writer(csvFile)
+
+
+        # creating some variables to store info
+        polarity = 0
+        positive = 0
+        wpositive = 0
+        spositive = 0
+        negative = 0
+        wnegative = 0
+        snegative = 0
+        neutral = 0
+
+
+        # iterating through tweets fetched
+        for tweet in self.tweets:
+            #Append to temp so that we can store in csv later. I use encode UTF-8
+            self.tweetText.append(self.cleanTweet(tweet.text).encode('utf-8'))
+            # print (tweet.text.translate(non_bmp_map))    #print tweet's text
+            analysis = TextBlob(tweet.text)
+            # print(analysis.sentiment)  # print tweet's polarity
+            polarity += analysis.sentiment.polarity  # adding up polarities to find the average later
+
+            if (analysis.sentiment.polarity == 0):  # adding reaction of how people are reacting to find average later
+                neutral += 1
+            elif (analysis.sentiment.polarity > 0 and analysis.sentiment.polarity <= 0.3):
+                wpositive += 1
+            elif (analysis.sentiment.polarity > 0.3 and analysis.sentiment.polarity <= 0.6):
+                positive += 1
+            elif (analysis.sentiment.polarity > 0.6 and analysis.sentiment.polarity <= 1):
+                spositive += 1
+            elif (analysis.sentiment.polarity > -0.3 and analysis.sentiment.polarity <= 0):
+                wnegative += 1
+            elif (analysis.sentiment.polarity > -0.6 and analysis.sentiment.polarity <= -0.3):
+                negative += 1
+            elif (analysis.sentiment.polarity > -1 and analysis.sentiment.polarity <= -0.6):
+                snegative += 1
+
+
+        # Write to csv and close csv file
+        csvWriter.writerow(self.tweetText)
+        csvFile.close()
+
+        # finding average of how people are reacting
+        positive = self.percentage(positive, NoOfTerms)
+        wpositive = self.percentage(wpositive, NoOfTerms)
+        spositive = self.percentage(spositive, NoOfTerms)
+        negative = self.percentage(negative, NoOfTerms)
+        wnegative = self.percentage(wnegative, NoOfTerms)
+        snegative = self.percentage(snegative, NoOfTerms)
+        neutral = self.percentage(neutral, NoOfTerms)
+
+        # finding average reaction
+        polarity = polarity / NoOfTerms
+
+        # printing out data
+        print("How people are reacting on " + searchTerm + " by analyzing " + str(NoOfTerms) + " tweets.")
+        print()
+        print("General Report: ")
+        finalreview=""
+
+        if (polarity == 0):
+            finalreview=("Neutral")
+        elif (polarity > 0 and polarity <= 0.3):
+            finalreview=("Weakly Positive")
+        elif (polarity > 0.3 and polarity <= 0.6):
+            finalreview=("Positive")
+        elif (polarity > 0.6 and polarity <= 1):
+            finalreview=("Strongly Positive")
+        elif (polarity > -0.3 and polarity <= 0):
+            finalreview=("Weakly Negative")
+        elif (polarity > -0.6 and polarity <= -0.3):
+            finalreview=("Negative")
+        elif (polarity > -1 and polarity <= -0.6):
+            finalreview=("Strongly Negative")
+
+        print()
+        print("Detailed Report: ")
+        pos=(str(positive) + "% people thought it was positive")
+        wpos=(str(wpositive) + "% people thought it was weakly positive")
+        spos=(str(spositive) + "% people thought it was strongly positive")
+        neg=(str(negative) + "% people thought it was negative")
+        wneg=(str(wnegative) + "% people thought it was weakly negative")
+        sneg=(str(snegative) + "% people thought it was strongly negative")
+        neu=(str(neutral) + "% people thought it was neutral")
+
+        #self.plotPieChart(positive, wpositive, spositive, negative, wnegative, snegative, neutral, searchTerm, NoOfTerms)
+        return([pos,wpos,spos,neg,wneg,sneg,neu,finalreview])
+
+
+    def cleanTweet(self, tweet):
+        # Remove Links, Special Characters etc from tweet
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) | (\w +:\ / \ / \S +)", " ", tweet).split())
+
+    # function to calculate percentage
+    def percentage(self, part, whole):
+        temp = 100 * float(part) / float(whole)
+        return format(temp, '.2f')
+
+    def plotPieChart(self, positive, wpositive, spositive, negative, wnegative, snegative, neutral, searchTerm, noOfSearchTerms):
+        labels = ['Positive [' + str(positive) + '%]', 'Weakly Positive [' + str(wpositive) + '%]','Strongly Positive [' + str(spositive) + '%]', 'Neutral [' + str(neutral) + '%]',
+                  'Negative [' + str(negative) + '%]', 'Weakly Negative [' + str(wnegative) + '%]', 'Strongly Negative [' + str(snegative) + '%]']
+        sizes = [positive, wpositive, spositive, neutral, negative, wnegative, snegative]
+        colors = ['yellowgreen','darkkhaki','darkolivegreen', 'aquamarine', 'red','lightsalmon','darkred']
+        patches, texts = plt.pie(sizes, colors=colors, startangle=110)
+        plt.legend(patches, labels, loc="best")
+        plt.title('How people are reacting on ' + searchTerm + ' by analyzing ' + str(noOfSearchTerms) + ' Tweets.')
+        plt.axis('equal')
+        plt.tight_layout()
+        plt.show()
+
+
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/addRegion', methods=['POST'])
+def addRegion():
+	st=request.form['projectFilePath']
+	nft=request.form['projectFilePath1']
+	print(nft)
+	sa = SentimentAnalysis()
+	sts="Detailed Report"+"\n"+"\n"
+	li=sa.DownloadData(st,nft)
+	print(len(li))
+	for i in range(0,len(li)-1):
+		sts=sts+"\n"+str(li[i])
+	return render_template('index.html',resultes=sts,finalreview="Final Review:"+"\n"+str(li[-1]))
+
+
+@app.route('/addRegion1', methods=['POST'])
+def addRegion1():
+    st = request.form['kwrd']
+    nft = request.form['nooft']
+    processed_text = st.upper()
+    sa = SentimentAnalysis()
+    li=sa.DownloadData(st,nft)
+    print(li)
+    return render_template('index.html',)
+
+if __name__ == '__main__':
+    #app.secret_key='secret123'
+    app.run(debug=True)
